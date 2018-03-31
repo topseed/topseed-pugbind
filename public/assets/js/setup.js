648c80e846befdb56820d98eba22c789c4a8069f
unknown <uptimevic@gmail.com>
2018-03-30 20:16:28 -0400

    now testing

--- public/assets/js/setup.js
+++ public/assets/js/setup.js
@@ -1,8 +1,7 @@
 
  // http://github.com/muicss/loadjs/issues/56
 
- // https://jsfiddle.net/muicss/4791kt3w/
-
+ // https://jsfiddle.net/muicss/4791kt3w
 function require(bundleIds, callbackFn) {
 	bundleIds.forEach(function(bundleId) {
 		if (!loadjs.isDefined(bundleId)) loadjs(bundles[bundleId], bundleId)
@@ -10,9 +9,19 @@ function require(bundleIds, callbackFn) {
 	loadjs.ready(bundleIds, callbackFn)
 }
 
+
+// this needs an ugly code fix, like a bundleReadyIE before PRELoadB.done() fires - this is not deterministic:
+if (!window.Promise) {
+	var js = document.createElement('script'); js.src='//cdn.jsdelivr.net/es6-promise-polyfill/1.2.0/promise.min.js'; document['head'].appendChild(js)
+}
+
+if (!window.fetch) {
+	var js = document.createElement('script'); js.src='//cdn.jsdelivr.net/fetch/2.0.1/fetch.min.js'; document['head'].appendChild(js)
+}
+
+
 loadjs([
 	'//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js',
-	'//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js',
 	'//cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
 ], 'PRELoadBx123', {
 	async: false //required due to loadjs bug with bundles
@@ -24,11 +33,13 @@ loadjs.ready('PRELoadBx123', function () {
 })
 
 
-// this needs a fix, like a bundleReadyIE before PRELoadB fires - this is not deterministic:
-if (!window.Promise) {
-	var js = document.createElement('script'); js.src='//cdn.jsdelivr.net/es6-promise-polyfill/1.2.0/promise.min.js'; document['head'].appendChild(js)
-}
+// ready
+loadjs.ready('PRELoadB', function() {
+	loadjs([ '//cdn.jsdelivr.net/npm/semantic-ui@2.3.0/dist/semantic.css',
+	'//cdn.jsdelivr.net/npm/semantic-ui@2.3.0/dist/semantic.js',
+	'//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
+	], 'semaB')
+})
 
-if (!window.fetch) {
-	var js = document.createElement('script'); js.src='//cdn.jsdelivr.net/fetch/2.0.1/fetch.min.js'; document['head'].appendChild(js)
-}
+
+console.log('setup v1.04.01')
