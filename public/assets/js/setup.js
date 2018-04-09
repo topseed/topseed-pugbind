a37c34d987ab426184f845e5324735688297c1ce
unknown <uptimevic@gmail.com>
2018-04-09 15:45:13 -0400

    site

--- public/assets/js/setup.js
+++ public/assets/js/setup.js
@@ -2,7 +2,7 @@
  // http://github.com/muicss/loadjs/issues/56
 
  // https://jsfiddle.net/muicss/4791kt3w
-function require(bundleIds, callbackFn) {
+ function require(bundleIds, callbackFn) {
 	bundleIds.forEach(function(bundleId) {
 		if (!loadjs.isDefined(bundleId)) loadjs(bundles[bundleId], bundleId)
 	})
@@ -32,33 +32,29 @@ loadjs.ready(['promise','fetch'], function () {
 	loadjs([
 		'//cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js'
 		,'//cdn.jsdelivr.net/npm/signals@1.0.0/dist/signals.min.js'
-		,'//ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'
 		//, '//cdn.jsdelivr.net/npm/riot@3.9.1/riot.js'
 	], 'core' /* bundle ID */, {
 		async: false //required due to loadjs bug with bundles
 	})
 })
-
-loadjs.ready(['core', 'domReady'], function () {
+loadjs.ready(['core'], function () {
 	//window['SITE'] = new signals.Signal() //site events
 	loadjs([ '//cdn.jsdelivr.net/npm/semantic-ui@2.3.0/dist/semantic.js'
-	], 'cssJs', {
+		], 'cssJs', {
 		async: false //required due to loadjs bug with bundles
 	})
-	setTimeout(function(){
-		$( document ).ready(function() {
-			loadjs.done('site') // "done with bundle 'site'", need this because we're not loading js here
-		})
-	}, 1)
-})
+	$( document ).ready(function() {
+		loadjs.done('site') // "done with bundle 'site'", need this because we're not loading js here
+	})
+})//()
 
 function cssLoaded() {// called by the style sheet in layout
 	console.log('css done')
-	loadjs.done('css') 
+	loadjs.done('css')
 }
 
-loadjs.ready(['css', 'cssJs'], function () {
-	loadjs.done('style') 
+loadjs.ready(['css', 'cssJs', 'site'], function () {
+	loadjs.done('style')
 })
 
 // usage: ////////////////////////////////////////////////////////////////////
